f1f6c9a6e65ea8c8e1de5f0f4e5e343b5bd75a2b
吴子豪 <vortez.wohl@gmail.com>
2025-04-27 21:10:57 +0800

    feat: 为 embedding 计算添加缓存

--- lotc/embedding/long_text_embedding.py
+++ lotc/embedding/long_text_embedding.py
@@ -5,22 +5,34 @@ import torch
 from sentence_transformers import SentenceTransformer
 
 from lotc import __ROOT__
+from lotc.util.hash import md5
 
+CACHE = dict()
 logger = logging.getLogger('lotc.embedding')
 
 
 def long_text_embedding(text: str, max_length: int,
-                        chunk_size: int = 128, bert_model: SentenceTransformer = None) -> tuple[int, torch.Tensor]:
+                        chunk_size: int = 128, bert_model_name_or_path: str = 'moka-ai/m3e-small') -> tuple[int, torch.Tensor]:
+
     def chunk_embedding(input_tup: tuple[int, str]) -> tuple[int, torch.Tensor]:
         return input_tup[0], bert_model.encode(input_tup[1])
 
+    # read cache
+    _text_hash = md5(text)
+    if _text_hash in CACHE.keys():
+        _text_cache_dict = CACHE[_text_hash]
+        if (_text_cache_dict.get('max_length') == max_length
+                and _text_cache_dict.get('chunk_size') == chunk_size
+                and _text_cache_dict.get('bert_model_name_or_path') == bert_model_name_or_path):
+            if 'result' in _text_cache_dict.keys():
+                return _text_cache_dict.get('result')
+
     _text_to_show = text.replace("\n", str())
     logger.debug(f'Embedding text \"{_text_to_show if len(_text_to_show) < 128 else _text_to_show[:128] + "..."}\".')
-    if bert_model is None:
-        bert_model = SentenceTransformer(
-            model_name_or_path='moka-ai/m3e-small',
-            cache_folder=f'{__ROOT__}\\.cache'
-        )
+    bert_model = SentenceTransformer(
+        model_name_or_path=bert_model_name_or_path,
+        cache_folder=f'{__ROOT__}\\.cache'
+    )
     _text = text.strip()
     if len(_text) < max_length:
         _text += '.' * (max_length - len(_text))
@@ -35,4 +47,11 @@ def long_text_embedding(text: str, max_length: int,
     fin_emb_tensor = torch.tensor([], dtype=torch.float32)
     for emb in fin_embedding:
         fin_emb_tensor = torch.cat((fin_emb_tensor, torch.tensor(emb, dtype=torch.float32)), dim=0)
+    # write cache
+    CACHE[_text_hash] = {
+        'max_length': max_length,
+        'chunk_size': chunk_size,
+        'bert_model_name_or_path': bert_model_name_or_path,
+        'result': (fin_emb_tensor.shape[-1], fin_emb_tensor)
+    }
     return fin_emb_tensor.shape[-1], fin_emb_tensor
---
+++ lotc/util/hash.py
@@ -0,0 +1,8 @@
+import hashlib
+
+
+def md5(text: str) -> str:
+    md5_hash = hashlib.md5()
+    text_bytes = text.encode('utf-8')
+    md5_hash.update(text_bytes)
+    return md5_hash.hexdigest()
