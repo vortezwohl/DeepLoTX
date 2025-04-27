3093b8c1b0f6632a7cbb24c7f9be749828ecf8bc
吴子豪 <vortez.wohl@gmail.com>
2025-04-27 18:33:25 +0800

    feat: 默认chunk size改为128, 最小线程改为8

--- ltc/embedding/long_text_embedding.py
+++ ltc/embedding/long_text_embedding.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer
 
 
 def long_text_embedding(text: str, max_length: int,
-                        chunk_size: int = 512, bert_model: SentenceTransformer = None) -> tuple[int, torch.Tensor]:
+                        chunk_size: int = 128, bert_model: SentenceTransformer = None) -> tuple[int, torch.Tensor]:
     def chunk_embedding(input_tup: tuple[int, str]) -> tuple[int, torch.Tensor]:
         return input_tup[0], bert_model.encode(input_tup[1])
 
@@ -18,7 +18,7 @@ def long_text_embedding(text: str, max_length: int,
     chunks = []
     for i in range(num_chunks):
         chunks.append((i, _text[i * chunk_size: (i + 1) * chunk_size]))
-    with ThreadPoolExecutor(max_workers=min(num_chunks + 1, 6)) as executor:
+    with ThreadPoolExecutor(max_workers=min(num_chunks + 1, 8)) as executor:
         embeddings = list(executor.map(chunk_embedding, chunks))
     embeddings.sort(key=lambda x: x[0])
     fin_embedding = [x[1] for x in embeddings]
