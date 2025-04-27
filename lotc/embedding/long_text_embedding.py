c82c2bfde3638a8921d6cf840a2fc6a229bfce08
吴子豪 <vortez.wohl@gmail.com>
2025-04-27 21:16:44 +0800

    feat: 为 embedding 计算添加缓存

--- lotc/embedding/long_text_embedding.py
+++ lotc/embedding/long_text_embedding.py
@@ -13,7 +13,6 @@ logger = logging.getLogger('lotc.embedding')
 
 def long_text_embedding(text: str, max_length: int,
                         chunk_size: int = 128, bert_model_name_or_path: str = 'moka-ai/m3e-small') -> tuple[int, torch.Tensor]:
-
     def chunk_embedding(input_tup: tuple[int, str]) -> tuple[int, torch.Tensor]:
         return input_tup[0], bert_model.encode(input_tup[1])
 
@@ -26,7 +25,6 @@ def long_text_embedding(text: str, max_length: int,
                 and _text_cache_dict.get('bert_model_name_or_path') == bert_model_name_or_path):
             if 'result' in _text_cache_dict.keys():
                 return _text_cache_dict.get('result')
-
     _text_to_show = text.replace("\n", str())
     logger.debug(f'Embedding text \"{_text_to_show if len(_text_to_show) < 128 else _text_to_show[:128] + "..."}\".')
     bert_model = SentenceTransformer(
@@ -47,11 +45,12 @@ def long_text_embedding(text: str, max_length: int,
     fin_emb_tensor = torch.tensor([], dtype=torch.float32)
     for emb in fin_embedding:
         fin_emb_tensor = torch.cat((fin_emb_tensor, torch.tensor(emb, dtype=torch.float32)), dim=0)
+    result = fin_emb_tensor.shape[-1], fin_emb_tensor
     # write cache
     CACHE[_text_hash] = {
         'max_length': max_length,
         'chunk_size': chunk_size,
         'bert_model_name_or_path': bert_model_name_or_path,
-        'result': (fin_emb_tensor.shape[-1], fin_emb_tensor)
+        'result': result
     }
-    return fin_emb_tensor.shape[-1], fin_emb_tensor
+    return result
