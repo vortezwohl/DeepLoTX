5fac5c9a5029a112bdf9dab22ae41f219aac1ded
吴子豪 <vortez.wohl@gmail.com>
2025-04-27 20:33:55 +0800

    优化日志显示

--- ltc/__init__.py
+++ ltc/__init__.py
@@ -5,9 +5,11 @@ __ROOT__ = os.path.dirname(os.path.abspath(__file__))
 
 logger = logging.getLogger('ltc')
 logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(name)s : %(message)s')
+
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(name)s : %(message)s')
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
