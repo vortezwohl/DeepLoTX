98f621a2bdee8415f94e8dea8a80472afb2145d3
吴子豪 <vortezwohl@proton.me>
2025-04-29 00:14:40 +0800

    feat: 添加 softmax 回归

---
+++ lotc/nn/softmax_regression.py
@@ -0,0 +1,14 @@
+from typing_extensions import override
+
+import torch
+
+from lotc.nn.linear_regression import LinearRegression
+
+
+class SoftmaxRegression(LinearRegression):
+    def __init__(self, input_dim: int, output_dim: int):
+        super().__init__(input_dim=input_dim, output_dim=output_dim)
+
+    @override
+    def forward(self, x) -> torch.Tensor:
+        return torch.softmax(super().forward(x), dim=-1, dtype=torch.float32)
