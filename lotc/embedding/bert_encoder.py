f632bf3b46e5d754cd0f4386937464bf5ed99dcc
吴子豪 <vortezwohl@proton.me>
2025-04-28 23:48:38 +0800

    feat: 实现 bert encoder

---
+++ lotc/embedding/bert_encoder.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+from transformers import BertTokenizer, BertModel
+
+from lotc import __ROOT__
+
+CACHE_PATH = f'{__ROOT__}\\.cache'
+
+
+class BertEncoder(nn.Module):
+    def __init__(self, model_name_or_path='bert-base-uncased'):
+        super().__init__()
+        self.tokenizer = BertTokenizer.from_pretrained(pretrained_model_name_or_path=model_name_or_path,
+                                                       cache_dir=CACHE_PATH)
+        self.bert = BertModel.from_pretrained(pretrained_model_name_or_path=model_name_or_path,
+                                              cache_dir=CACHE_PATH)
+
+    def forward(self, input_ids) -> torch.Tensor:
+        self.bert.eval()
+        with torch.no_grad():
+            res = self.bert.forward(input_ids)
+        return res
+
+    # noinspection PyUnresolvedReferences
+    def encode(self, text: str) -> torch.Tensor:
+        _input_ids = self.tokenizer.encode(text, return_tensors='pt')
+        return self.forward(_input_ids).last_hidden_state[:, 0, :].squeeze()
