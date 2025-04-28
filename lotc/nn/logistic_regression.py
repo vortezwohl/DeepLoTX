86b0a5c4b898c7f5199017dba8c69779973d4232
吴子豪 <vortez.wohl@gmail.com>
2025-04-28 21:08:58 +0800

    feat: 逻辑回归支持多元逻辑回归

--- lotc/nn/logistic_regression.py
+++ lotc/nn/logistic_regression.py
@@ -6,8 +6,8 @@ from lotc.nn.linear_regression import LinearRegression
 
 
 class LogisticRegression(LinearRegression):
-    def __init__(self, input_dim: int):
-        super().__init__(input_dim=input_dim, output_dim=1)
+    def __init__(self, input_dim: int, output_dim: int = 1):
+        super().__init__(input_dim=input_dim, output_dim=output_dim)
 
     @override
     def forward(self, x) -> torch.Tensor:
