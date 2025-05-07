d3223fc20f85c5f8aed2baf507051c0e1ff1454d
吴子豪 <vortez.wohl@gmail.com>
2025-05-07 16:38:06 +0800

    feat: 支持 l1 正则化

--- lotc/nn/base_neural_network.py
+++ lotc/nn/base_neural_network.py
@@ -9,21 +9,21 @@ class BaseNeuralNetwork(nn.Module):
             if model_name is not None \
             else self.__class__.__name__
 
-    def l1_regularization(self, _lambda: float = 0.) -> torch.Tensor:
-        def __l1_regularize() -> torch.Tensor:
+    def l1(self, _lambda: float = 0.) -> torch.Tensor:
+        def _l1() -> torch.Tensor:
             l2_reg = torch.tensor(0.)
             for param in self.parameters():
                 l2_reg += (torch.abs(param)).sum()
             return l2_reg
-        return _lambda * __l1_regularize()
+        return _lambda * _l1()
 
-    def l2_regularization(self, _lambda: float = 0.) -> torch.Tensor:
-        def __l2_regularize() -> torch.Tensor:
+    def l2(self, _lambda: float = 0.) -> torch.Tensor:
+        def _l2() -> torch.Tensor:
             l2_reg = torch.tensor(0.)
             for param in self.parameters():
                 l2_reg += (torch.pow(param, exponent=2.)).sum()
             return l2_reg
-        return _lambda * __l2_regularize()
+        return _lambda * _l2()
 
     def forward(self, x) -> torch.Tensor: ...
 
