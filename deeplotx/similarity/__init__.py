5e73f933dd2fbc56abbc4d48d6be5704e6aede2a
吴子豪 <vortez.wohl@gmail.com>
2025-05-10 18:18:24 +0800

    feat: 优化ndarray适配器

--- deeplotx/similarity/__init__.py
+++ deeplotx/similarity/__init__.py
@@ -4,15 +4,14 @@ import torch
 bias = 1e-12
 
 
-def ndarray_adapter(p: any, q: any) -> tuple[np.ndarray, np.ndarray]:
-    match p.__class__:
-        case torch.Tensor:
-            p = p.detach().cpu().numpy()
-        case List:
-            p = np.asarray(p)
-    match q.__class__:
-        case torch.Tensor:
-            q = q.detach().cpu().numpy()
-        case List:
-            q = np.asarray(q)
-    return p, q
+def ndarray_adapter(*args) -> tuple | np.ndarray:
+    args = list(args)
+    for i, arg in enumerate(args):
+        match arg.__class__:
+            case torch.Tensor:
+                args[i] = arg.detach().cpu().numpy()
+            case List:
+                args[i] = np.asarray(arg)
+    if len(args) > 1:
+        return tuple(args)
+    return args[0]
