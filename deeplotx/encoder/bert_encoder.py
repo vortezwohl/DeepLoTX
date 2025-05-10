6a2343dd089e93e7fc9d3645ad7c522abf77b1f2
吴子豪 <vortez.wohl@gmail.com>
2025-05-10 13:49:06 +0800

    feat: 优化 BERT 模型的输入维度适配

--- deeplotx/encoder/bert_encoder.py
+++ deeplotx/encoder/bert_encoder.py
@@ -13,19 +13,20 @@ class BertEncoder(nn.Module):
     def __init__(self, model_name_or_path: str = DEFAULT_BERT):
         super().__init__()
         self.tokenizer = BertTokenizer.from_pretrained(pretrained_model_name_or_path=model_name_or_path,
-                                                       cache_dir=CACHE_PATH)
+                                                       cache_dir=CACHE_PATH, _from_auto=True)
         self.bert = BertModel.from_pretrained(pretrained_model_name_or_path=model_name_or_path,
-                                              cache_dir=CACHE_PATH)
+                                              cache_dir=CACHE_PATH, _from_auto=True)
+        self.embed_dim = self.bert.config.max_position_embeddings
 
     def forward(self, input_ids, attention_mask: torch.Tensor) -> torch.Tensor:
         def _encoder(_input_tup: tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
             return self.bert.forward(_input_tup[0], attention_mask=_input_tup[1]).last_hidden_state[:, 0, :]
 
-        num_chunks = math.ceil(input_ids.shape[-1] / 512)
+        num_chunks = math.ceil(input_ids.shape[-1] / self.embed_dim)
         chunks = chunk_results = []
         for i in range(num_chunks):
-            start_idx = i * 512
-            end_idx = min(start_idx + 512, input_ids.shape[-1])
+            start_idx = i * self.embed_dim
+            end_idx = min(start_idx + self.embed_dim, input_ids.shape[-1])
             chunks.append((input_ids[:, start_idx: end_idx], attention_mask[:, start_idx: end_idx]))
         ori_mode = self.bert.training
         self.bert.eval()
