3d846d17154c0c64a977d76bb59289dfb6e2d69f
吴子豪 <vortez.wohl@gmail.com>
2025-05-10 18:06:54 +0800

    feat: 添加ndarray适配器

--- deeplotx/similarity/__init__.py
+++ deeplotx/similarity/__init__.py
@@ -1,1 +1,18 @@
+import numpy as np
+import torch
+
 bias = 1e-12
+
+
+def ndarray_adapter(p: any, q: any) -> tuple[np.ndarray, np.ndarray]:
+    match p.__class__:
+        case torch.Tensor:
+            p = p.detach().cpu().numpy()
+        case List:
+            p = np.asarray(p)
+    match q.__class__:
+        case torch.Tensor:
+            q = q.detach().cpu().numpy()
+        case List:
+            q = np.asarray(q)
+    return p, q
--- deeplotx/similarity/distribution.py
+++ deeplotx/similarity/distribution.py
@@ -1,26 +1,31 @@
 import numpy as np
+import torch
 
-from deeplotx.similarity import bias
+from deeplotx.similarity import bias, ndarray_adapter
 
 
-def cross_entropy(p: np.ndarray, q: np.ndarray) -> np.float32:
+def cross_entropy(p: torch.Tensor | np.ndarray, q: torch.Tensor | np.ndarray) -> np.float32:
+    p, q = ndarray_adapter(p, q)
     q = np.clip(q, bias, 1 - bias)
     return -1 * (np.sum(p * np.log(q)) / p.shape[0])
 
 
-def kl_divergence(p: np.ndarray, q: np.ndarray) -> np.float32:
+def kl_divergence(p: torch.Tensor | np.ndarray, q: torch.Tensor | np.ndarray) -> np.float32:
+    p, q = ndarray_adapter(p, q)
     q = np.where(q == 0, bias, q)
     p = p / np.sum(p)
     q = q / np.sum(q)
     return np.sum(p * np.log(p / q))
 
 
-def js_divergence(p: np.ndarray, q: np.ndarray) -> np.float32:
+def js_divergence(p: torch.Tensor | np.ndarray, q: torch.Tensor | np.ndarray) -> np.float32:
+    p, q = ndarray_adapter(p, q)
     m = (p + q) / 2
     return (kl_divergence(p, m) + kl_divergence(q, m)) / 2
 
 
-def hellinger_distance(p: np.ndarray, q: np.ndarray) -> np.float32:
+def hellinger_distance(p: torch.Tensor | np.ndarray, q: torch.Tensor | np.ndarray) -> np.float32:
+    p, q = ndarray_adapter(p, q)
     p = p / np.sum(p)
     q = q / np.sum(q)
     squared_diff = (np.sqrt(p) - np.sqrt(q)) ** 2
