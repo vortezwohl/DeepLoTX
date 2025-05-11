96395969706461cfe33691b56f9d8918c77f4518
吴子豪 <vortezwohl@proton.me>
2025-05-11 15:44:06 +0800

    update: 整理包结构

--- deeplotx/nn/__init__.py
+++ deeplotx/nn/__init__.py
@@ -1,3 +1,5 @@
 from .linear_regression import LinearRegression
 from .logistic_regression import LogisticRegression
 from .softmax_regression import SoftmaxRegression
+from .recursive_sequential import RecursiveSequential
+from .auto_regression import AutoRegression
