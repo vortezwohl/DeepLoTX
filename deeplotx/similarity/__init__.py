7c7cd6e75d0dadd7cb00096141a0024866ef6d36
吴子豪 <vortezwohl@proton.me>
2025-05-11 16:15:29 +0800

    update: 更新包结构

--- deeplotx/similarity/__init__.py
+++ deeplotx/similarity/__init__.py
@@ -15,3 +15,8 @@ def ndarray_adapter(*args) -> tuple | np.ndarray:
     if len(args) > 1:
         return tuple(args)
     return args[0]
+
+
+from .set import jaccard_similarity, ochiai_similarity, dice_coefficient, overlap_coefficient
+from .vector import euclidean_similarity, cosine_similarity, chebyshev_similarity, l2_normalize, z_score_normalize
+from .distribution import cross_entropy, kl_divergence, js_divergence, hellinger_distance
