9d433de527c25a83afb752136ac82d199fdf31f9
吴子豪 <vortez.wohl@gmail.com>
2025-06-11 20:18:57 +0800

    update: 优化 trainer API

--- deeplotx/trainer/text_binary_classification_trainer.py
+++ deeplotx/trainer/text_binary_classification_trainer.py
@@ -6,7 +6,7 @@ from torch import nn, optim
 from torch.utils.data import DataLoader, TensorDataset
 
 from deeplotx.encoder.long_text_encoder import LongTextEncoder
-from deeplotx.nn.logistic_regression import LogisticRegression
+from deeplotx.nn.recursive_sequential import RecursiveSequential
 from deeplotx.trainer.base_trainer import BaseTrainer
 
 logger = logging.getLogger('deeplotx.trainer')
@@ -20,20 +20,22 @@ class TextBinaryClassifierTrainer(BaseTrainer):
 
     @override
     def train(self, positive_texts: list[str], negative_texts: list[str],
-              num_epochs: int, learning_rate: float = 2e-5, balancing_dataset: bool = True,
+              num_epochs: int, learning_rate: float = 2e-6, balancing_dataset: bool = True,
               train_loss_threshold: float = 0.0, valid_loss_threshold: float = 0.0,
-              alpha: float = 1e-4, rho: float = 0.2) -> LogisticRegression:
+              alpha: float = 1e-4, rho: float = 0.2,
+              hidden_dim: int = 256, recursive_layers: int = 2) -> RecursiveSequential:
         if balancing_dataset:
             min_length = min(len(positive_texts), len(negative_texts))
             positive_texts = positive_texts[:min_length]
             negative_texts = negative_texts[:min_length]
         all_texts = positive_texts + negative_texts
-        labels = ([torch.tensor([1.0], dtype=torch.float32, device=self.device) for _ in range(len(positive_texts))]
-                  + [torch.tensor([0.0], dtype=torch.float32, device=self.device) for _ in range(len(negative_texts))])
-        text_embeddings = [self._long_text_encoder.encode(x) for x in all_texts]
+        text_embeddings = [self._long_text_encoder.encode(x, flatten=False, use_cache=True) for x in all_texts]
         feature_dim = text_embeddings[0].shape[-1]
-        inputs = torch.stack(text_embeddings)
-        labels = torch.stack(labels)
+        dtype = text_embeddings[0].dtype
+        labels = ([torch.tensor([1.], dtype=dtype, device=self.device) for _ in range(len(positive_texts))]
+                  + [torch.tensor([.0], dtype=dtype, device=self.device) for _ in range(len(negative_texts))])
+        inputs = torch.stack(text_embeddings).to(self.device)
+        labels = torch.stack(labels).to(self.device)
         dataset_size = len(labels)
         train_size = int(self._train_ratio * dataset_size)
         train_dataset = TensorDataset(inputs[:train_size], labels[:train_size])
@@ -44,7 +46,9 @@ class TextBinaryClassifierTrainer(BaseTrainer):
             logger.warning("The dimension of features doesn't match. A new model instance will be created.")
             self.model = None
         if self.model is None:
-            self.model = LogisticRegression(input_dim=feature_dim, output_dim=1)
+            self.model = RecursiveSequential(input_dim=feature_dim, output_dim=1,
+                                             hidden_dim=hidden_dim,
+                                             recursive_layers=recursive_layers)
         self.model.to(self.device)
         loss_function = nn.BCELoss()
         optimizer = optim.Adamax(self.model.parameters(), lr=learning_rate)
@@ -52,7 +56,7 @@ class TextBinaryClassifierTrainer(BaseTrainer):
             self.model.train()
             total_loss = 0.0
             for batch_texts, batch_labels in train_loader:
-                outputs = self.model.forward(batch_texts)
+                outputs = torch.sigmoid(self.model.forward(batch_texts, self.model.initial_state(batch_texts.shape[0]))[0])
                 loss = loss_function(outputs, batch_labels) + self.model.elastic_net(alpha=alpha, rho=rho)
                 optimizer.zero_grad()
                 loss.backward()
@@ -63,7 +67,7 @@ class TextBinaryClassifierTrainer(BaseTrainer):
                 for batch_texts, batch_labels in valid_loader:
                     with torch.no_grad():
                         self.model.eval()
-                        outputs = self.model.forward(batch_texts)
+                        outputs = torch.sigmoid(self.model.forward(batch_texts, self.model.initial_state(batch_texts.shape[0]))[0])
                         loss = loss_function(outputs, batch_labels) + self.model.elastic_net(alpha=alpha, rho=rho)
                         total_valid_loss += loss.item()
                         self.model.train()
