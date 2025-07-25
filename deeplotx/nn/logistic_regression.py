d59e0c7cd55042711ebd450729bae57b9a3156f6
吴子豪 <vortez.wohl@gmail.com>
2025-07-25 11:42:11 +0800

    feat: 改进逻辑回归模型

--- deeplotx/nn/logistic_regression.py
+++ deeplotx/nn/logistic_regression.py
@@ -6,9 +6,12 @@ from deeplotx.nn.linear_regression import LinearRegression
 
 
 class LogisticRegression(LinearRegression):
-    def __init__(self, input_dim: int, output_dim: int = 1, model_name: str | None = None,
+    def __init__(self, input_dim: int, output_dim: int = 1, num_layers: int = 1, expansion_factor: int | float = 1.25,
+                 bias: bool = True, dropout_rate: float = 0.1, model_name: str | None = None,
                  device: str | None = None, dtype: torch.dtype | None = None):
-        super().__init__(input_dim=input_dim, output_dim=output_dim, model_name=model_name, device=device, dtype=dtype)
+        super().__init__(input_dim=input_dim, output_dim=output_dim, num_layers=num_layers,
+                         expansion_factor=expansion_factor, bias=bias, dropout_rate=dropout_rate,
+                         model_name=model_name, device=device, dtype=dtype)
 
     @override
     def forward(self, x: torch.Tensor) -> torch.Tensor:
