6252ff9fa6ab7467a910de3d70f1e10fddfb14e1
吴子豪 <vortezwohl@proton.me>
2025-07-30 01:39:17 +0800

    feat: lte 支持变长序列

--- deeplotx/encoder/long_text_encoder.py
+++ deeplotx/encoder/long_text_encoder.py
@@ -13,9 +13,9 @@ logger = logging.getLogger('deeplotx.embedding')
 
 
 class LongTextEncoder(Encoder):
-    def __init__(self, max_length: int, chunk_size: int = 448,
-                 overlapping: int = 32, model_name_or_path: str = DEFAULT_BERT,
-                 cache_capacity: int = 64, max_workers: int = 8, device: str | None = None):
+    def __init__(self, chunk_size: int = 448, overlapping: int = 32, max_length: int = -1,
+                 model_name_or_path: str = DEFAULT_BERT, cache_capacity: int = 64,
+                 max_workers: int = 8, device: str | None = None):
         super().__init__(model_name_or_path=model_name_or_path, device=device)
         assert overlapping < chunk_size, f'overlapping ({overlapping}) must be less than chunk size ({chunk_size}).'
         self._max_length = max_length
@@ -41,23 +41,28 @@ class LongTextEncoder(Encoder):
                 _fin_emb_tensor = torch.cat((_fin_emb_tensor.detach().clone(), _emb.detach().clone()), dim=-1)
             return _fin_emb_tensor.squeeze()
 
+        _tmp_max_length = self._max_length
         _text_to_show = text.replace("\n", str())
         logger.debug(f'Embedding \"{_text_to_show if len(_text_to_show) < 128 else _text_to_show[:128] + "..."}\".')
         # read cache
         _text_hash = sha512(text)
         if _text_hash in self._cache:
             return postprocess(self._cache[_text_hash], flatten)
-        _text_to_input_ids = self.tokenizer.encode(text.strip())[:self._max_length]
+        _text_to_input_ids = self.tokenizer.encode(text.strip())
+        # variable length
+        if _tmp_max_length < 0:
+            _tmp_max_length = len(_text_to_input_ids)
+        _text_to_input_ids = _text_to_input_ids[:_tmp_max_length]
         _text_to_input_ids_att_mask = []
         # padding
         pad_token = self.tokenizer.pad_token_type_id
-        if len(_text_to_input_ids) < self._max_length:
-            _text_to_input_ids.extend([pad_token] * (self._max_length - len(_text_to_input_ids)))
+        if len(_text_to_input_ids) < _tmp_max_length:
+            _text_to_input_ids.extend([pad_token] * (_tmp_max_length - len(_text_to_input_ids)))
         pads = _text_to_input_ids.count(pad_token)
-        non_pads = self._max_length - pads
+        non_pads = _tmp_max_length - pads
         _text_to_input_ids_att_mask.extend([1] * non_pads)
         _text_to_input_ids_att_mask.extend([0] * pads)
-        num_chunks = math.ceil(self._max_length / self._chunk_size)
+        num_chunks = math.ceil(_tmp_max_length / self._chunk_size)
         # split chunks
         chunks = []
         for i in range(num_chunks):
